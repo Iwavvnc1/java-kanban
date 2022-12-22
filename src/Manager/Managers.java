d8f41f03e8efa686f333963f67617ddf3622202b
Iwavvnc1 <popigbang@yandex.ru>
2022-12-22 18:08:04 +0500

    исправлены ошибки в интерфейсе TaskManager.java,

--- src/Manager/Managers.java
+++ src/Manager/Managers.java
@@ -1,8 +1,8 @@
 package Manager;
 
 public class Managers {
-    public static TaskManager getDefault() { //меня зовут Иван, Игорь это случайное имя в аккаунте
-
+    public static TaskManager getDefault() { /*меня зовут Иван, Игорь это случайное имя в аккаунте,
+  и с наступающим новым годом)*/
         return new InMemoryTaskManager();
     }
     public static InMemoryHistoryManager  getDefaultHistory() {
